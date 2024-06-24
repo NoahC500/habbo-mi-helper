1e41f1468c31550d0c0e32129833a59380ba49a7
Noah Craig <noahdcraig@outlook.com>
2024-06-24 02:12:57 +0100

    Operation cleanup complete

--- Script.py
+++ Script.py
@@ -19,7 +19,9 @@ elif sys.platform == 'win32':
 elif sys.platform == 'darwin':
     openCommand = "open"
 
-def mainMenu():  # Main Menu GUI
+def mainMenu(root):  # Main Menu GUI
+    if root != "":
+        root.destroy()
     menuGUI = Tk()
     menuGUI.title("MI Login Checker")
     frame = Frame(menuGUI, padding=10)
@@ -35,7 +37,7 @@ def mainMenu():  # Main Menu GUI
     mottoCheckerBtn = Button(menuGUI, text='Check Mottos', command=lambda: mottoChecker(menuGUI), width=40)
     mottoCheckerBtn.grid(column=0, row=2)
 
-    editNamesBtn = Button(menuGUI, text='Edit Names', command=lambda: editNameGUI(menuGUI), width=40)
+    editNamesBtn = Button(menuGUI, text='Edit Names', command=lambda: editNamesGUI(menuGUI), width=40)
     editNamesBtn.grid(column=0, row=3)
 
     getImagesBtn = Button(menuGUI, text='Get Images', command=lambda: getImages(), width=40)
@@ -50,13 +52,12 @@ def mainMenu():  # Main Menu GUI
     menuGUI.mainloop()
 
 def loginChecker(root):  # Checks Last Login Time
-    global nameList
     lastLoginStr = []
     onlineStatus = []
     lastAccessTime = []
     profileVisible = []
 
-    readFile()
+    nameList = readFile()
 
     for i in range(0, len(nameList)):
         response = requests.get(f"https://www.habbo.com/api/public/users?name={nameList[i]}")
@@ -65,34 +66,36 @@ def loginChecker(root):  # Checks Last Login Time
         onlineStatus.append(response.get('online'))
         lastAccessTime.append(response.get('lastAccessTime'))
 
-        lastLoginStrFunc(i, lastLoginStr, profileVisible, onlineStatus, lastAccessTime)
+    lastLoginStr= lastLoginStrFunc(lastLoginStr, profileVisible, onlineStatus, lastAccessTime, nameList)
 
-def onlineGUI(root):  # GUI for 'Login Checker'
-    global nameList
-    tryKill(root)
+    loginCheckerGUI(root, lastLoginStr, nameList)
+
+
+def loginCheckerGUI(root, lastLoginStr, nameList):  # GUI for 'Login Checker'
+    root.destroy()
     root = Tk()
     root.title("MI Login Checker")
     root.resizable(width=False, height=False)
     frame = Frame(root, padding=10)
     frame.grid()
-    for i in range(0, len(nameList)):
-        # print(f"string length is {len(string)}\nstring is {string}\ni is {i}")  # Only for testing
-        Label(frame, text=(f"{string[i]}")).grid(column=0, row=i)
 
-    menuButton = Button(root, text="Menu", command=lambda: online2main(root), width=40)
-    menuButton.grid(column=0, row=len(nameList)+1)
-    refreshButton = Button(root, text="Refresh", command=lambda: refresh(root), width=40)
-    refreshButton.grid(column=0, row=2)
+    for i in range(0, len(nameList)):
+        Label(frame, text=(f"{lastLoginStr[i]}")).grid(column=0, row=i)
+
+    refreshBtn = Button(root, text="Refresh", command=lambda: loginChecker(root), width=40)
+    refreshBtn.grid(column=0, row=len(nameList))
+
+    menuBtn = Button(root, text="Menu", command=lambda: mainMenu(root), width=40)
+    menuBtn.grid(column=0, row=len(nameList)+1)
+
     root.mainloop()
 
-def readFile():  # Gets names from file, splitting into an array
-    global nameList
-    nameList = open(f"{home}/.Habbo-Name-List/habbo-eedb-names.txt")
-    nameList = (nameList.read()).split()
+def readFile():  # Gets names from file, then splits into an array
+    nameList = ((open(f"{home}/.Habbo-Name-List/habbo-eedb-names.txt")).read()).split()
+    return nameList
 
-def lastLoginStrFunc(i, lastLoginStr, profileVisible, onlineStatus, lastAccessTime):  # Function to determine correct string to show on 'Login Checker' page
-    global nameList
-    for i in range(0, len(nameList)):
+def lastLoginStrFunc(lastLoginStr, profileVisible, onlineStatus, lastAccessTime, nameList):  # Function to determine correct string to show on 'Login Checker' page
+    for i in range(0,len(nameList)):
         if onlineStatus[i] == True:
             lastLoginStr.append(f"{nameList[i]} is online")
 
@@ -102,8 +105,6 @@ def lastLoginStrFunc(i, lastLoginStr, profileVisible, onlineStatus, lastAccessTi
         elif lastAccessTime[i] == None:
             lastLoginStr.append(f"{nameList[i]}'s online status is private")
 
-        elif onlineStatus[i] != 'true' and lastAccessTime[i] != None:
-            lastLoginStrFunc(i, lastLoginStr)
         else:
             days, hours = calcTimeDelta(str(lastAccessTime[i]).split('.')[0])
             if days == 1 and hours == 1:
@@ -116,6 +117,7 @@ def lastLoginStrFunc(i, lastLoginStr, profileVisible, onlineStatus, lastAccessTi
                 lastLoginStr.append(f"{nameList[i]} last logged in {days} days and {hours} hours ago")
             if days > 3:
                 lastLoginStr[i] = f"{lastLoginStr[i]} DUE CHECKUP"
+    return lastLoginStr
 
 def calcTimeDelta(fromDate):  # Calculates time since last online
     fromDate = datetime.strptime(fromDate, '%Y-%m-%dT%H:%M:%S')
@@ -124,127 +126,93 @@ def calcTimeDelta(fromDate):  # Calculates time since last online
     hours = (int(delta.total_seconds()) // 3600) % 24
     return days, hours
 
-def editNameGUI(root):
-    global nameList
+def editNamesGUI(root):
     root.destroy()
-    readFile()
-    editRoot = Tk()
-    editRoot.title("MI Login Checker")
-    editRoot.resizable(width=False, height=False)
-    frame = Frame(editRoot, padding=10)
+    nameList = readFile()
+    root = Tk()
+    root.title("MI Login Checker")
+    root.resizable(width=False, height=False)
+    frame = Frame(root, padding=10)
     frame.grid()
-    text = Text(editRoot, width=40, height=10)
-    text.grid(column=0, row=0)
-    # text.insert(1.0, f"this is a test")  # Only for testing
+    namesInput = Text(root, width=40, height=10)
+    namesInput.grid(column=0, row=0)
+
     for i in range(0, len(nameList)):
-        text.insert(f'{i+1}.0', f"{nameList[i]}\n")
-    done = Button(editRoot, text='Done', command=lambda: doneNameChange(editRoot, text), width=40)
-    done.grid(column=0, row=1)
-    cancel = Button(editRoot, text='Cancel', command=lambda: edit2main(editRoot), width=40)
-    cancel.grid(column=0, row=2)
-    editRoot.mainloop()
+        namesInput.insert(f'{i+1}.0', f"{nameList[i]}\n")
 
-def edit2main(root):
-    tryKill(root)
-    mainMenu()
+    doneBtn = Button(root, text='Done', command=lambda: saveNames(root, namesInput), width=40)
+    doneBtn.grid(column=0, row=1)
 
-def doneNameChange(editRoot, text):
-    global nameList
-    # print("doneNameChange()")  # Only for testing
-    # temp = text.get(1.0, END)  # Only for testing
-    # print(temp) # Only for testing
+    cancelBtn = Button(root, text='Cancel', command=lambda: mainMenu(root), width=40)
+    cancelBtn.grid(column=0, row=2)
+
+    root.mainloop()
+
+def saveNames(root, namesInput):
     nameList = open(f"{home}/.Habbo-Name-List/habbo-eedb-names.txt", 'w')
-    nameList.write(text.get(1.0, END))
+    nameList.write(namesInput.get(1.0, END))
     nameList.close()
-    editRoot.destroy()
-    readFile()
-    mainMenu()
+    mainMenu(root)
 
 def openDQ():
-    global nameList
-    readFile()
+    nameList = readFile()
     for i in range(0, len(nameList)):
         webbrowser.open(f"https://habbouk.com/dq/stats/{nameList[i]}")
 
-def online2main(root):
-    root.destroy()
-    mainMenu()
-
-def tryKill(root):
-    try:
-        root.destroy()
-    except:
-        print("Tried to kill, failed")
-
 def getImages():
-    global nameList
-    readFile()
-    try:
-        os.system(f"rm {home}/.Habbo-Name-List/images/*")
-    except:
-        print("No images in folder")
-    for i in range(0,len(nameList)):
-        response = requests.get(f"https://www.habbo.com/habbo-imaging/avatarimage?user={nameList[i]}&action=wav&direction=2&head_direction=2&gesture=sml&size=m&img_format=gif")
-        response = io.BytesIO(response.content)
-        image = Image.open(response)
+    nameList = readFile()
+
+    subprocess.run(f"rm {home}/.Habbo-Name-List/images/*", shell=True) # Deletes all images currently in folder
+
+    for i in range(0,len(nameList)): # Habbo waving
         imgPath = f"{home}/.Habbo-Name-List/images/{nameList[i]}.gif"
-        image.save(imgPath, format="GIF")
-    for i in range(0,len(nameList)):
-        response = requests.get(f"https://www.habbo.com/habbo-imaging/avatarimage?user={nameList[i]}&direction=3&head_direction=3&gesture=sml&drk=42&size=l")
-        response = io.BytesIO(response.content)
-        image = Image.open(response)
+        (Image.open(io.BytesIO((requests.get(f"https://www.habbo.com/habbo-imaging/avatarimage?user={nameList[i]}&action=wav&direction=2&head_direction=2&gesture=sml&size=m&img_format=gif")).content))).save(imgPath, format="GIF")
+
+    for i in range(0,len(nameList)): # Habbo facing forward
         imgPath = f"{home}/.Habbo-Name-List/images/{nameList[i]} forward.png"
-        image.save(imgPath, format="PNG")
-    for i in range(0,len(nameList)):
-        response = requests.get(f"https://www.habbo.com/habbo-imaging/avatarimage?user={nameList[i]}&action=crr=6&direction=&head_direction=2&gesture=sml&size=s&img_format=gif")
-        response = io.BytesIO(response.content)
-        image = Image.open(response)
-        imgPath = f"{home}/.Habbo-Name-List/images/{nameList[i]} classic.png"
-        image.save(imgPath, format="GIF")
+        (Image.open(io.BytesIO((requests.get(f"https://www.habbo.com/habbo-imaging/avatarimage?user={nameList[i]}&direction=3&head_direction=3&gesture=sml&drk=42&size=l")).content))).save(imgPath, format="PNG")
+
+    for i in range(0,len(nameList)): # Old-style Habbo w/ Coffee
+        imgPath = f"{home}/.Habbo-Name-List/images/{nameList[i]} classic & coffee.gif"
+        (Image.open(io.BytesIO((requests.get(f"https://www.habbo.com/habbo-imaging/avatarimage?user={nameList[i]}&action=crr=6&direction=&head_direction=2&gesture=sml&size=s&img_format=gif")).content))).save(imgPath, format="GIF")
+
+    for i in range(0,len(nameList)): # Old-style Habbo w/ Coffee
+        imgPath = f"{home}/.Habbo-Name-List/images/{nameList[i]} classic.gif"
+        (Image.open(io.BytesIO((requests.get(f"https://www.habbo.com/habbo-imaging/avatarimage?user={nameList[i]}&direction=&head_direction=2&gesture=sml&size=s&img_format=gif")).content))).save(imgPath, format="GIF")
 
 def openImagesDir():
     global openCommand
-    subprocess.Popen([openCommand, f"{home}/.Habbo-Name-List/images/"])
+    subprocess.run([openCommand, f"{home}/.Habbo-Name-List/images/"])
 
 def gitPull():  # Currently disabled by default, kinda broken, will add to enable syncing list between computers
     os.system(f"cd {home}/.Habbo-Name-List/script && git pull")
     os.system(f"cd {home}/.Habbo-Name-List/ && git pull")
 
-def refresh(root):
-    onlineMain()
-    root.destroy()
-    onlineGUI(root)
-
-def refreshMotto(root):
-    mottoChecker(root)
-
-def start():
-    # gitPull()
-    onlineMain()
-    onlineGUI()
-
 def mottoChecker(root):
-    global nameList, motto
-    readFile()
+    nameList = readFile()
     motto = []
+
     for i in range(0, len(nameList)):
-        response = requests.get(f"https://www.habbo.com/api/public/users?name={nameList[i]}")
-        response = response.json()
-        # print(f"{nameList[i]}\n{response.get('motto')}")
-        motto.append(f"{nameList[i]}: {response.get('motto')}")
-        print(motto[i])
+        userInfo = (requests.get(f"https://www.habbo.com/api/public/users?name={nameList[i]}")).json()
+        motto.append(f"{nameList[i]}: {userInfo.get('motto')}")
+
     root.destroy()
     root = Tk()
     root.title("MI Login Checker")
     root.resizable(width=False, height=False)
     frame = Frame(root, padding=10)
     frame.grid()
+
     for i in range(0,len(nameList)):
         Label(frame, text=(motto[i])).grid(column=0, row=i)
-    menuButton = Button(root, text="Menu", command=lambda: online2main(root), width=40)
+
+    menuButton = Button(root, text="Menu", command=lambda: mainMenu(root), width=40)
     menuButton.grid(column=0, row=len(nameList)+1)
-    refreshButton = Button(root, text="Refresh", command=lambda: refreshMotto(root), width=40)
+
+    refreshButton = Button(root, text="Refresh", command=lambda: mottoChecker(root), width=40)
     refreshButton.grid(column=0, row=2)
+
     root.mainloop()
 
-mainMenu()
+root = "" # Means there need not be a kill before mainMenu() function
+mainMenu(root)
